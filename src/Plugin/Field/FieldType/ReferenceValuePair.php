588b7d260ff14502e361038b6bcf7b42ff547436
miiimooo <miiimooo@62954.no-reply.drupal.org>
2016-10-11 17:05:23 +0200

    Issue #2805783 by miiimooo, rivedav: Reference Value Pair conflict with Taxonomy Term (Entity Reference) Field type

--- src/Plugin/Field/FieldType/ReferenceValuePair.php
+++ src/Plugin/Field/FieldType/ReferenceValuePair.php
@@ -583,28 +583,6 @@ class ReferenceValuePair extends EntityReferenceItem {
    * {@inheritdoc}
    */
   public static function getPreconfiguredOptions() {
-    $options = array();
-
-    // Add all the commonly referenced entity types as distinct pre-configured
-    // options.
-    $entity_types = \Drupal::entityManager()->getDefinitions();
-    $common_references = array_filter($entity_types, function (EntityTypeInterface $entity_type) {
-      return $entity_type->isCommonReferenceTarget();
-    });
-
-    /** @var \Drupal\Core\Entity\EntityTypeInterface $entity_type */
-    foreach ($common_references as $entity_type) {
-      $options[$entity_type->id()] = [
-        'label' => $entity_type->getLabel(),
-        'field_storage_config' => [
-          'settings' => [
-            'target_type' => $entity_type->id(),
-          ],
-        ],
-      ];
-    }
-
-    return $options;
+    return array();
   }
-
 }
